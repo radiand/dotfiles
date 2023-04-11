9797a7c0d8efb6366bd90dc5b67f9a32f920fad4
radiand <radiand@protonmail.com>
2023-04-11 16:07:34 +0200

    refactor(nvim): removed toggle diagnostics (gz) mapping

--- config/.config/nvim/lua/51-lsp.lua
+++ config/.config/nvim/lua/51-lsp.lua
@@ -76,22 +76,3 @@ vim.diagnostic.config({
     update_in_insert = false,
     virtual_text = false,
 })
-
-vim.g.diagnostics_visible = true
-function _G.toggle_diagnostics()
-    if vim.g.diagnostics_visible then
-        vim.g.diagnostics_visible = false
-        vim.diagnostic.disable()
-    else
-        vim.g.diagnostics_visible = true
-        vim.diagnostic.enable()
-    end
-end
-
-vim.api.nvim_buf_set_keymap(
-    0,
-    "n",
-    "gz",
-    ":call v:lua.toggle_diagnostics()<CR>",
-    { silent = true, noremap = true }
-)
