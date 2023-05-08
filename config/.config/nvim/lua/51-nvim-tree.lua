cf2f83968f693527654ebba5f7e0c0c0ec974fb0
radiand <radiand@protonmail.com>
2023-05-08 15:30:04 +0200

    fix(nvim): added ? mapping for nvim-tree

--- config/.config/nvim/lua/51-nvim-tree.lua
+++ config/.config/nvim/lua/51-nvim-tree.lua
@@ -1,4 +1,25 @@
+local function nvim_tree_custom_attach(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+        return {
+            desc = "nvim-tree: " .. desc,
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            nowait = true,
+        }
+    end
+
+    -- use all default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- add some
+    vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+end
+
 require("nvim-tree").setup({
+    on_attach = nvim_tree_custom_attach,
     view = {
         width = 40,
     },
