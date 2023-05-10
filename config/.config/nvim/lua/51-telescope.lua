9bca0aa83b4438e7c0d05faa2ba163beddf71ecd
radiand <radiand@protonmail.com>
2023-05-10 13:25:24 +0200

    feat(nvim): ability to delete buffers from telescope

--- config/.config/nvim/lua/51-telescope.lua
+++ config/.config/nvim/lua/51-telescope.lua
@@ -4,3 +4,17 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
+
+require("telescope").setup({
+    pickers = {
+        buffers = {
+            show_all_buffers = true,
+            -- Use <C-d> to delete buffers within buffers picker.
+            mappings = {
+                i = {
+                    ["<c-d>"] = "delete_buffer",
+                },
+            },
+        },
+    },
+})
