dc3bb24dc82d4ee7b36458540d106129eaa2f5a5
radiand <radiand@protonmail.com>
2023-05-18 19:03:14 +0200

    feat(nvim): added quickfix and loclist mappings for telescope

--- config/.config/nvim/lua/51-telescope.lua
+++ config/.config/nvim/lua/51-telescope.lua
@@ -2,7 +2,8 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fl", builtin.loclist, {})
+vim.keymap.set("n", "<leader>fq", builtin.quickfix, {})
 vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
 
 require("telescope").setup({
